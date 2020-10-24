1895e7a18cd4d46002b5f6aa461eae059e10b7fd
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-24 15:35:16 -0500

    Send test email

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,8 +1,32 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'us-west-1' });
+
 async function sendMail(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
+  const params = {
+    Source: 'reyes.yerp@gmail.com',
+    Destination: {
+      ToAddresses: ['reyes.yerp@hotmail.com'],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from AWS SES',
+        },
+      },
+      Subject: {
+        Data: 'Test Mail',
+      },
+    },
   };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
